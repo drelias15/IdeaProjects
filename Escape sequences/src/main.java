f84e8e81ff83dbe9566d25560b03767bc638ad20
drelias15 <drelias15@gmail.com>
2018-07-11 16:00:46 -0400

    Updates

---
+++ Escape sequences/src/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main( String[] args ){
+
+
+
+        System.out.println("\t \t \t *");
+        System.out.println("\b\t \t  * \t*");
+        System.out.println("\b \b \t \t* \t \t  *");
+        System.out.println("\b\t \t  * \t*");
+        System.out.println("\b \t \t \t *");
+
+
+    }
+}
