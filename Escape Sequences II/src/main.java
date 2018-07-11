4d8c099b1103d28c52e9c4b1dcdb68498ef5e129
drelias15 <drelias15@gmail.com>
2018-07-11 17:28:59 -0400

    updates

---
+++ Escape Sequences II/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main( String[] args ){
+
+
+
+        System.out.println("Cost\t\tQuantity\t\tTotal ");
+        System.out.println("$1,000.00\t\t4\t\t$4,000.00 ");
+        System.out.println("$\b\t50.00\t\t8\t\t  $400.00 ");
+        System.out.println("__________________________________");
+        System.out.println("\tTotal\t\t12\t\t$4,400.00 ");
+    }
+}
