50d0fea912356c3aaa07ba19da277bd0a3c4fbf5
drelias15 <drelias15@gmail.com>
2018-07-12 09:29:44 -0400

    Updates

---
+++ Math Two/src/MathTwo.java
@@ -0,0 +1,12 @@
+public class MathTwo {
+
+    public static void main ( String[] args){
+
+        int num1 = 2;
+        float num2 = 1.7938F;
+
+        System.out.println("The first number is " + num1);
+        System.out.println("The second number is " + num2);
+
+    }
+}
